fea9370c069775bc45dc0516cad9aa7b52d71c03
Corey Hellwege <coreyhellwege@gmail.com>
2020-01-11 19:56:02 +1100

    connect database

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,33 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 // app
 const app = express();
 
+// database
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Database connected");
+  });
+
 // middleware
 app.use(morgan("dev")); // gives us access to endpoints in the console
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+
+// cors
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
 // routes
 app.get("/api", (req, res) => {
