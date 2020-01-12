45143b48ebdeb81dec1d4dd947207d33328053b5
Corey Hellwege <coreyhellwege@gmail.com>
2020-01-12 16:20:39 +1100

    create new user object

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,30 @@
+const User = require("../models/user");
+const shortId = require("shortid");
+
 exports.signup = (req, res) => {
-  // get data from the body of the request
-  const { name, email, password } = req.body;
-  res.json({
-    user: { name, email, password }
+  // check if username alraedy exists
+  User.findOne({ email: req.body.email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "Email is taken"
+      });
+    }
+
+    // if user doesn't exist yet, create new user
+    const { name, email, password } = req.body;
+    let username = shortId.generate(); // generate random username
+    let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+    let newUser = new User({ name, email, password, profile, username });
+
+    newUser.save((error, success) => {
+      if (error) {
+        return res.status(400).json({
+          error: error
+        });
+      }
+      res.json({
+        message: "Signup successful! Sign in"
+      });
+    });
   });
 };
