4bc8eb96808d40b65597f27a3e961973dbe37d4f
Corey Hellwege <coreyhellwege@gmail.com>
2020-01-12 15:53:48 +1100

    create password encryption

--- backend/models/user.js
+++ backend/models/user.js
@@ -53,5 +53,46 @@ const userSchema = new mongoose.Schema(
   { timestamp: true }
 );
 
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    // create a temporary var to store pass
+    this._password = password;
+    // generate salt
+    this.salt = this.makeSalt();
+    // encrypt
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+// create the methods
+userSchema.methods = {
+  // compare encrypted pass with hashed pass in the db
+  authenticate: function(plainText) {
+    // hash the plain text pass
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  // hash the password
+  encryptPassword: function(password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+
+  // generate salt
+  makeSalt: function() {
+    return Math.round(new Date().valueOf() * Math.random() + "");
+  }
+};
+
 // export the model and name it 'User'
 module.exports = mongoose.model("User", userSchema);
