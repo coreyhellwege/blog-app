fa12bbe927b602ba76e04dd6dc7b45725970f609
Corey Hellwege <coreyhellwege@gmail.com>
2020-01-27 19:12:35 +1100

    install nprogress package and load cdn

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { APP_NAME } from "../config";
 import Link from "next/link";
 import Router from "next/router";
+import NProgress from "nprogress";
 import { signout, isAuth } from "../actions/auth";
 import {
   Collapse,
@@ -17,6 +18,11 @@ import {
   DropdownItem,
   NavbarText
 } from "reactstrap";
+
+// progress bar
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
