7ca1c089d0c502948a1dd7074dd49b61877f4e1a
Corey Hellwege <coreyhellwege@gmail.com>
2020-02-02 22:49:42 +1100

    start building out the CreateBlog form

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -7,13 +7,74 @@ import { isAuth, getCookie } from "../../actions/auth";
 import { getCategories } from "../../actions/category";
 import { getTags } from "../../actions/tag";
 import { createBlog } from "../../actions/blog";
-const ReactQuill = dynamic(() => import("react-quill", { ssr: false })); // dynamic import
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 const CreateBlog = ({ router }) => {
+  // state
+  const [body, setBody] = useState({});
+  const [values, setValues] = useState({
+    error: "",
+    sizeError: "",
+    success: "",
+    formData: "",
+    title: "",
+    hidePublishButton: false
+  });
+
+  // destructure state properties
+  const {
+    error,
+    sizeError,
+    success,
+    formData,
+    title,
+    hidePublishButton
+  } = values;
+
+  const publishBlog = e => {
+    e.preventDefault(); // so page doesn't refresh
+    console.log("ready to publish blog");
+  };
+
+  const handleChange = name => e => {
+    console.log(e.target.value);
+  };
+
+  const handleBody = e => {
+    console.log(e);
+  };
+
+  const createBlogForm = () => {
+    return (
+      <form onSubmit={publishBlog}>
+        <div className="form-group">
+          <label className="text-muted">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            value={title}
+            onChange={handleChange("title")}
+          />
+        </div>
+        <div className="form-group">
+          <ReactQuill
+            value={body}
+            placeholder="Write something amazing..."
+            onChange={handleBody}
+          />
+        </div>
+        <div>
+          <button type="submit" className="btn btn-primary">
+            Publish
+          </button>
+        </div>
+      </form>
+    );
+  };
   return (
     <div>
-      <h2>Create Blog Form</h2>
-      {JSON.stringify(router)}; // see what the router is
+      {createBlogForm()}
+      {/* {JSON.stringify(router)}; // see what the router is */}
     </div>
   );
 };
