d35afd4876f8a65cdd421b8d7fd6bcd2e5150039
Corey Hellwege <coreyhellwege@gmail.com>
2020-02-09 20:42:24 +1100

    create seo head section for blogs - not working

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -1,12 +1,40 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/layout";
+import { withRouter } from "next/router";
 import { useState } from "react";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import Card from "../../components/blog/Card";
+import { API, DOMAIN, APP_NAME, FB_APP_ID } from "../../config";
 
 // props are accessed from the getInitialProps() method
-const Blogs = ({ blogs, categories, tags, size }) => {
+const Blogs = ({ blogs, categories, tags, size, router }) => {
+  const head = () => {
+    <Head>
+      <title>Climate Facts | {APP_NAME}</title>
+      <meta name="description" content="Climate Science for dummies" />
+      <link rel="canonical" href={`${DOMAIN}${router.pathname}`} />
+      <meta
+        property="og:title"
+        content={`Climate Science for dummies | ${APP_NAME}`}
+      />
+      <meta property="og:description" content="Climate Science for dummies" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={`${DOMAIN}${router.pathname}`} />
+      <meta property="og:site_name" content={`${APP_NAME}`} />
+      <meta
+        property="og:image"
+        content={`${DOMAIN}/public/images/climatefacts.jpg`}
+      />
+      <meta
+        property="og:image:secure_url"
+        content={`${DOMAIN}/public/images/climatefacts.jpg`}
+      />
+      <meta property="og:image:type" content="image/jpg" />
+      <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+    </Head>;
+  };
+
   const showAllBlogs = () => {
     return blogs.map((blog, i) => {
       return (
@@ -39,31 +67,34 @@ const Blogs = ({ blogs, categories, tags, size }) => {
   };
 
   return (
-    <Layout>
-      <main>
-        <div className="container-fluid">
-          <header>
-            <div className="col-md-12 pt-3">
-              <h1 className="display-4 font-weight-bold text-center">
-                Climate Facts
-              </h1>
-            </div>
-            <section>
-              <div className="pb-5 text-center">
-                {showAllCategories()}
-                <br />
-                {showAllTags()}
+    <React.Fragment>
+      {head()}
+      <Layout>
+        <main>
+          <div className="container-fluid">
+            <header>
+              <div className="col-md-12 pt-3">
+                <h1 className="display-4 font-weight-bold text-center">
+                  Climate Facts
+                </h1>
               </div>
-            </section>
-          </header>
-        </div>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12">{showAllBlogs()}</div>
+              <section>
+                <div className="pb-5 text-center">
+                  {showAllCategories()}
+                  <br />
+                  {showAllTags()}
+                </div>
+              </section>
+            </header>
           </div>
-        </div>
-      </main>
-    </Layout>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-12">{showAllBlogs()}</div>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </React.Fragment>
   );
 };
 
@@ -83,4 +114,4 @@ Blogs.getInitialProps = () => {
   });
 };
 
-export default Blogs; // getInitialProps for server side rendering
+export default withRouter(Blogs); // getInitialProps for server side rendering
