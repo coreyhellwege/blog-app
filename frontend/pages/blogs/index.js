6dec2ebdb4232b2e903582e59fb493a560d1d760
Corey Hellwege <coreyhellwege@gmail.com>
2020-02-15 15:10:52 +1100

    display 2 blogs by default and create loadMore blogs option

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -8,7 +8,15 @@ import Card from "../../components/blog/Card";
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from "../../config";
 
 // props are accessed from the getInitialProps() method
-const Blogs = ({ blogs, categories, tags, size, router }) => {
+const Blogs = ({
+  blogs,
+  categories,
+  tags,
+  totalBlogs,
+  blogsLimit,
+  blogSkip,
+  router
+}) => {
   const head = () => (
     <Head>
       <title>Climate Facts | {APP_NAME}</title>
@@ -35,6 +43,37 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
     </Head>
   );
 
+  //state
+  const [limit, setLimit] = useState(blogsLimit);
+  const [skip, setSkip] = useState(0);
+  const [size, setSize] = useState(totalBlogs);
+  const [loadedBlogs, setLoadedBlogs] = useState([]);
+
+  const loadMore = () => {
+    let toSkip = skip + limit;
+    listBlogsWithCategoriesAndTags(toSkip, limit).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        // merge more blogs into blogs already loaded
+        setLoadedBlogs([...loadedBlogs, ...data.blogs]);
+        setSize(data.size);
+        setSkip(toSkip);
+      }
+    });
+  };
+
+  const loadMoreButton = () => {
+    return (
+      size > 0 &&
+      size >= limit && (
+        <button onClick={loadMore} className="btn btn-primary btn-lg">
+          Load more
+        </button>
+      )
+    );
+  };
+
   const showAllBlogs = () => {
     return blogs.map((blog, i) => {
       return (
@@ -44,6 +83,14 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
         </article>
       );
     });
+  };
+
+  const showLoadedBlogs = () => {
+    return loadedBlogs.map((blog, i) => (
+      <article key={i}>
+        <Card blog={blog} />
+      </article>
+    ));
   };
 
   const showAllCategories = () => {
@@ -87,11 +134,9 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
               </section>
             </header>
           </div>
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-md-12">{showAllBlogs()}</div>
-            </div>
-          </div>
+          <div className="container-fluid">{showAllBlogs()}</div>
+          <div className="container-fluid">{showLoadedBlogs()}</div>
+          <div className="text-center pt-5 pb-5">{loadMoreButton()}</div>
         </main>
       </Layout>
     </React.Fragment>
@@ -100,7 +145,9 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
 
 // nextJS lifecycle method
 Blogs.getInitialProps = () => {
-  return listBlogsWithCategoriesAndTags().then(data => {
+  let skip = 0;
+  let limit = 2;
+  return listBlogsWithCategoriesAndTags(skip, limit).then(data => {
     if (data.error) {
       console.log(data.error);
     } else {
@@ -108,7 +155,9 @@ Blogs.getInitialProps = () => {
         blogs: data.blogs,
         categories: data.categories,
         tags: data.tags,
-        size: data.size
+        totalBlogs: data.size,
+        blogsLimit: limit,
+        blogSkip: skip
       };
     }
   });
