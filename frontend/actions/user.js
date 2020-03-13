629a41c13442035dd830bebac217739eceb9773c
Corey Hellwege <coreyhellwege@gmail.com>
2020-03-13 21:43:06 +1100

    create profile update actions, page and component

--- frontend/actions/user.js
+++ frontend/actions/user.js
@@ -1,12 +1,42 @@
 import fetch from "isomorphic-fetch";
 import { API } from "../config";
 
-// endpoints
+// get user public profile
 export const userPublicProfile = username => {
   return fetch(`${API}/user/${username}`, {
     method: "GET",
     headers: {
       Accept: "application/json"
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+// get profile
+export const getProfile = token => {
+  return fetch(`${API}/user/profile`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+// update profile
+export const update = (token, user) => {
+  return fetch(`${API}/user/update`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
     }
   })
     .then(response => {
--- frontend/pages/user/index.js
+++ frontend/pages/user/index.js
@@ -6,7 +6,31 @@ const UserIndex = () => {
   return (
     <Layout>
       <Private>
-        <h2>User Dashboard</h2>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12 pt-5 pb-5">
+              <h2>User Dashboard</h2>
+            </div>
+            <div className="col-md-4">
+              <ul className="list-group">
+                <li className="list-group-item">
+                  {/* use an anchor tag here instead of Link to force the page to reload */}
+                  {/* so we know the text editor will load it's CSS properly */}
+                  <a href="/user/crud/blog">Create Blog</a>
+                </li>
+                <li className="list-group-item">
+                  <Link href="/user/crud/blogs">
+                    <a>Update/Delete Blogs</a>
+                  </Link>
+                </li>
+                <li className="list-group-item">
+                  <a href="/user/update">Update Profile</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-8">right</div>
+          </div>
+        </div>
       </Private>
     </Layout>
   );
