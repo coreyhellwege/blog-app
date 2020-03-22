b8e4d0dbcac54c67c539774398c031acce57bcb3
Corey Hellwege <coreyhellwege@gmail.com>
2020-03-22 17:48:27 +1100

    create route and controller method for contacting blog author

--- backend/controllers/form.js
+++ backend/controllers/form.js
@@ -28,3 +28,32 @@ exports.contactForm = (req, res) => {
     });
   });
 };
+
+exports.contactBlogAuthorForm = (req, res) => {
+  const { authorEmail, email, name, message } = req.body;
+  // console.log(req.body);
+
+  let maillist = [authorEmail, process.env.EMAIL_TO];
+
+  const emailData = {
+    to: maillist,
+    from: email,
+    subject: `Someone messaged you from - ${process.env.APP_NAME}`,
+    text: `Email received from contact form \n Sender name: ${name} \n Sender email: ${email} \n Sender message: ${message}`,
+    html: `
+        <h4>Message received from:</h4>
+        <p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+        <p>Message: ${message}</p>
+        <hr />
+        <p>This email may contain sensitive information</p>
+        <p>https://climatefacts.com</p>
+      `
+  };
+
+  sgMail.send(emailData).then(sent => {
+    return res.json({
+      success: true
+    });
+  });
+};
