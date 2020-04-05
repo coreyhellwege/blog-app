21d185022d2a1eabf8e8f007e1c92282c5ef62f5
Corey Hellwege <coreyhellwege@gmail.com>
2020-04-05 16:11:02 +1000

    add google analytics

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import getConfig from "next/config";
+const { publicRuntimeConfig } = getConfig();
 
 class MyDocument extends Document {
+  setGoogleTags() {
+    if (publicRuntimeConfig.PRODUCTION) {
+      return {
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'UA-162791629-1');
+        `
+      };
+    }
+  }
   render() {
     return (
       <Html lang="en">
@@ -23,6 +38,12 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.7/quill.snow.min.css"
           /> */}
           <link rel="stylesheet" href="/css/styles.css" />
+          {/* Google analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-162791629-1"
+          ></script>
+          <script dangerouslySetInnerHTML={this.setGoogleTags()}></script>
         </Head>
         <body>
           <Main />
