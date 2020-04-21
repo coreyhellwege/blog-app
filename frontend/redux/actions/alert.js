b56c9c69ab2d9c3253273c9d97397a1c5b568d62
Corey Hellwege <coreyhellwege@gmail.com>
2020-04-21 20:50:31 +1000

    add setTimeout to remove alert after 5s

--- frontend/redux/actions/alert.js
+++ frontend/redux/actions/alert.js
@@ -1,11 +1,13 @@
 import { SET_ALERT, REMOVE_ALERT } from "./types";
 const shortId = require("shortid");
 
-export const setAlert = (msg, alertType) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
   // create a random id
   const id = shortId.generate();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id },
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
